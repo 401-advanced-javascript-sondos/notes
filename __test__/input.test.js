19d95a13c0fb758d0b4cc3eeef5aecfc5bd2fcfb
SondosMatahen <sondosmatahen@gmail.com>
2020-09-07 18:27:07 +0300

    class02

---
+++ __test__/input.test.js
@@ -0,0 +1,43 @@
+'use strict';
+
+jest.mock('minimist');
+const minimist = require('minimist');
+minimist.mockImplementation(()=> {
+    return {
+        // a: 'Note',
+        //add:'make a note,
+    }
+});
+
+const Input = require('../lib/input.js');
+
+describe('Input Module', () => {
+
+    it('getAdd() will default to Enter a valid action when method has no value', ()=> {
+        let options = new Input();
+        expect(options.getAdd()).toEqual('Enter a valid action');
+    });
+
+    it('getAdd() will have valid methods when specified', ()=> {
+        let options = new Input();
+        expect(options.getAdd('a')).toEqual('a');
+        expect(options.getAdd('add')).toEqual('add');
+    });
+
+    it('Valid() respects proper object', ()=> {
+        let options = new Input();
+        expect(options.valid()).toBeTruthy();
+    });
+
+    it('getNote() will default to Error: invalid note when method has no value', ()=> {
+        let options = new Input();
+        expect(options.getNote()).toEqual('Error: invalid note');
+    });
+
+    it('getNote() returns my Note if valid', ()=> {
+        let options = new Input();
+        let note = 'http://foo.com';
+        expect(options.getNote(note)).toBeTruthy();
+    });
+    
+});
---
+++ __test__/notes.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let mocdata={action:'add',
+paload:'add note'}
+
+const Notes = require('../lib/notes.js');
+
+// Spies!
+// we will get to know if something was called or not.
+jest.spyOn(global.console, 'log');
+
+// describe the module I am testing
+describe('Notes Module', ()=> {
+    // test case
+    it('excute() does nothing with invalid options', ()=> {
+        const note = new Notes();
+        note.execute();
+        expect(console.log).not.toHaveBeenCalled();
+    });
+
+    it('excute() logs out when given options', ()=> {
+        const note = new Notes();
+        note.execute({opts: 'foo'});
+        expect(console.log).toHaveBeenCalled();
+    });
+
+});
+
